30672bb1f0c00f625e75b445f70d6378b93d6764
Hacuub <jkempsch000@gmail.com>
2021-02-08 15:29:08 -0500

    fixed

--- src/imageResponses.js
+++ src/imageResponses.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const image = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
 
 const getImage = (request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'image/png'});
     response.write(image);
     response.end();
 };
